bc74b51257f519014cc31a8b2a15b611ec7baebd
Li Chongxin <1136142252@qq.com>
2021-12-12 15:51:04 +0800

    1.5

--- js/fun.js
+++ js/fun.js
@@ -3,5 +3,17 @@ function js_method(){
 }
 
 function nopic(){
-        alert("没有更多图片了喔~")
+        alert("没有更多图片了喔~");
+}
+
+function nofun(){
+        alert("功能还未完善喔，抱歉~,暂不支持搜索鸭~");
+}
+
+function js_playhot(){
+        document.getElementById('yequ').play();
+}
+
+function nof(){
+        alert("功能还未完善喔,抱歉鸭~");
 }
